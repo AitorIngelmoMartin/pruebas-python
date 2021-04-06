109b56ae9224b2186e77e04394ea5cdfc2f8120f
Aitor Portatil <aitoringelmomartin@gmail.com>
2021-04-06 16:37:48 +0200

    He agregado otro archivo python para trabajar con condicionales

---
+++ condicionales_1.py
@@ -0,0 +1,10 @@
+numero1 = 50
+numero2 = 6
+
+if numero1>numero2 :
+    print('el número más grande es el:')
+    print(numero1)
+
+if numero2>numero1 :
+        print('el número más grande es el:')
+        print(numero2)
