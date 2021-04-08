19a775b183b3dbe9562a8395f796d60e2248b353
Aitor Ingelmo <aitorete2000@gmail.com>
2021-04-08 10:00:42 +0200

    He estado jugando con try y except

--- pruebas.py
+++ pruebas.py
@@ -2,4 +2,17 @@ print('hola amigos')
 
 numero = 5
 numero =str (numero)
-print ("Tu nunero es: "+numero)
+print ("Tu número es: "+numero)
+
+numero1 = input("Escribe tu número a continuación: ")
+4
+try:
+    numero1 = int (numero1)
+    print("El número escrito es el: ", numero1) 
+except:
+    print("La conversión ha fallado....")
+
+
+if numero1 < 20:
+    print("tu numero es menor que 20......")
+    
