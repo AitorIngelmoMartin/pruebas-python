cd9bdaa502c094df04b10a2489164ef8806962bf
Aitor Ingelmo <aitorete2000@gmail.com>
2021-04-09 10:02:24 +0200

    He jugado con una sentencia if y un comparador logico

--- pruebas.py
+++ pruebas.py
@@ -4,15 +4,20 @@ numero = 5
 numero =str (numero)
 print ("Tu número es: "+numero)
 
-numero1 = input("Escribe tu número a continuación: ")
+numero1 = input("Escribe tu número1 a continuación: ")
+numero2 = input("Escribe tu número2 a continuación: ")
 4
 try:
     numero1 = int (numero1)
-    print("El número escrito es el: ", numero1) 
+    numero2 = int (numero2) 
 except:
     print("La conversión ha fallado....")
 
 
-if numero1 < 20:
-    print("tu numero es menor que 20......")
-    
+if (0>numero1 or numero1>11) or (0>numero2 or numero2>11):
+    print("Alguno de tus números no está entre [0, 10]")
+
+
+print (numero1)
+print (numero2)
+print("--------------------------------------------------------------------------------------------------")
